2b78f7df1da557eb43f5da197d2b479cf6a157bd
Silas Buarque <silasbuarque@gmail.com>
2025-06-09 23:17:53 -0300

    New project files

---
+++ src/main/java/com/algaworks/algasensors/device/managament/DeviceManagamentApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.device.managament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagamentApplication.class, args);
+	}
+
+}
